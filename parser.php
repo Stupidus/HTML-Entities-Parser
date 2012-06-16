01d327ab3d9a14a11e0845a35696af026a374b1e
Victor Antoine <victor-antoine@live.fr>
2012-06-16 14:51:11 +0200

    repository creation

---
+++ parser.php
@@ -0,0 +1,7 @@
+<?php
+if(isset($_POST["texte"]) && !empty($_POST["texte"]))
+        $texte = $_POST["texte"];
+else
+        $texte = "";
+echo stripslashes(htmlentities($texte, ENT_QUOTES, "UTF-8"));
+?>

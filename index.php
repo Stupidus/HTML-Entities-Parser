2d9e905654c96effc9c8845644ceccb89bac89e9
Victor Antoine <victor-antoine@live.fr>
2015-02-02 19:13:43 +0100

    Translation to english

--- index.php
+++ index.php
@@ -8,16 +8,13 @@
 	<h1>HTML Entites Parser</h1>
 	<article>
 		<p>
-			Transformez un texte brut en entit&eacute;s HTML pour 
-			&eacute;viter tout probl&egrave;me d&#039;encodage ou 
-			pour pourvoir &eacute;crire du code source 
-			non-interpr&eacute;table par les navigateurs
+			Parse a raw text in HTML entities to avoid any encoding problem or to display non-interpretable source code for browsers
 		</p>
 		<textarea id="ParserIn" style="width:99%;" rows="10" name="texte"></textarea>
 		<br/>
-		<fieldset><legend>Sortie</legend><span id="ParserTexte"></span></fieldset>
+		<fieldset><legend>Output</legend><span id="ParserTexte"></span></fieldset>
 		<br/>
-		<input type="button" id="ParserParse" value="Parser le texte"/>
+		<input type="button" id="ParserParse" value="Parse text"/>
 	</article>
 	<script type="text/javascript">		
 		$("#ParserParse").click(function(){        
